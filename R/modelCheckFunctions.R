18907d7688bb36e2373a088e240692fb3c9dbabd
Gabriel Franco <gabrielfranco89@gmail.com>
2019-12-11 16:45:09 -0300

    Minor fixes

--- R/modelCheckFunctions.R
+++ R/modelCheckFunctions.R
@@ -2,15 +2,16 @@
 #'
 #' @name plotMC
 #' @param object aggrmodel object fit
+#' @param scales Character value indicating if scales should be 'fixed' or 'free' in the ggplot
 #'
 #' @return ggplot plot object
-plotMC <- function(object){
+plotMC <- function(object, scales = 'fixed'){
     require(ggplot2)
     mcMtx <- object$mc
     ## Plot
     p <- ggplot(aes(x=time, y=mc), data=mcMtx) +
         geom_line() +
-        facet_wrap(.~type)
+        facet_wrap(.~type, scales = scales)
     p
 }
 
