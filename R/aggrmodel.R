2b2e99d408b107836e51eec5d92128691e2d6e18
Gabriel Franco <gabrielfranco89@gmail.com>
2020-02-05 12:12:01 -0300

    Add surface with timeVar2 option, but getting bad estimatives of mean curves

--- R/aggrmodel.R
+++ R/aggrmodel.R
@@ -35,11 +35,16 @@ aggrmodel <- function(formula=NULL,
                       market,
                       Y = NULL,
                       timeVar,
+                      timeVar2=NULL,
                       groupVar,
                       repVar,
                       n_basis,
                       n_basis_cov = NULL,
                       basisFunction = 'B-Splines',
+                      timeVec2 = NULL,
+                      n_basis2 = NULL,
+                      n_order2 = NULL,
+                      basisFunction2 = NULL,
                       cicleRep = FALSE,
                       n_order = 4,
                       covType = 'Homog_Uniform',
@@ -68,6 +73,7 @@ aggrmodel <- function(formula=NULL,
     y = data[[substitute(Y)]]
     t = data[[substitute(timeVar)]]
     t = t/max(t)
+    if(!is.null(timeVar2)) t2 <- data[[substitute(timeVar2)]]
     grps = as.factor(data[[substitute(groupVar)]])
     reps = as.factor(data[[substitute(repVar)]])
     J = length(unique(grps))
@@ -78,18 +84,26 @@ aggrmodel <- function(formula=NULL,
                      rep=as.integer(reps),
                      time=t,
                      y = y)
+    if(!is.null(timeVar2)) dd$time2 <- t2
     dd <- dd[order(dd$group, dd$rep, dd$time),]
     y <- dd$y
     colnames(market) <- c("group","type","num")
     market$group <- as.integer(factor(market$group,
                                       levels=levels(grps)))
-
-    ## Build disgragation basis expansion design matrix
-    XList <- buildX(market=market,
+    ## Build disagragation basis expansion design matrix
+    mktLong <- spread(market,type,num)
+    groupnrep <- subset(dd, select=c(group,rep))
+    mktLong <- merge(groupnrep,mktLong)
+    X <- buildX(market=mktLong,
+                    nType = C,
                     timeVec = t,
                     n_basis = n_basis,
                     n_order = n_order,
-                    basis = basisFunction)
+                    basis = basisFunction,
+                    timeVec2 = t2,
+                    n_basis2 = n_basis2,
+                    n_order2 = n_order2,
+                    basis2 = basisFunction2)
     if(!is.null(formula)){
         cvrtMtx <- as.data.frame(model.matrix(formula, data = data))
         cvrtMtx <- cbind(cvrtMtx,
@@ -116,7 +130,7 @@ aggrmodel <- function(formula=NULL,
         rm(cvrtMtx)
     } ## end if is.null(formula)
     ## Get initial values -------------------------------------
-    init <- get_inits(XList=XList, I=I, y=y, C=C,
+    init <- get_inits(X=X, I=I, y=y, C=C,
                       covType=covType,
                       market=market,
                       corPar_init=corPar_init,
@@ -125,7 +139,7 @@ aggrmodel <- function(formula=NULL,
                       n_basis=n_basis, n_basis_cov=n_basis_cov,
                       t=t, n_order=n_order, basisFunc=basisFunc
                       )
-    X <- init$X
+    # X <- init$X
     beta_init <- init$beta
     parIn <- init$par
     lowerBoundVec <- init$lb
@@ -267,27 +281,77 @@ aggrmodel <- function(formula=NULL,
         lkIn <- lkOut
     } ## End while(lkDiff > diffTol)
 
-    ## Output Mean curves
-    if(basisFunction=='B-Splines')
-        basisObj = create.bspline.basis(range(t),
-                                        nbasis = n_basis,
-                                        norder = n_order)
-    if(basisFunction=='Fourier'){
-        basisObj = create.fourier.basis(range(t),
-                                        nbasis = n_basis)
+    ## Output ----------------------
+    if(is.null(timeVar2)){
+        if(basisFunction=='B-Splines')
+            basisObj = create.bspline.basis(range(t),
+                                            nbasis = n_basis,
+                                            norder = n_order)
+        if(basisFunction=='Fourier'){
+            basisObj = create.fourier.basis(range(t),
+                                            nbasis = n_basis)
+        }
+        tuni <- unique(t)
+        B = predict(basisObj, tuni)
+        ## Separate betas
+        betaMC <- betaOut[1:(C*n_basis)]
+        betaMtx <- cbind(beta=as.matrix(betaMC),
+                         type=rep(1:C, each=n_basis))
+        mcMtx <- tapply(betaMtx[,1],
+                        betaMtx[,2],
+                        function(x) B %*% x)
+        mcMtx <- data.frame(mc=unlist(mcMtx),
+                            time=rep(tuni,times=C),
+                            type=rep(unique(market[,2]), each=length(tuni)))
     }
-    tuni <- unique(t)
-    B = predict(basisObj, tuni)
-    ## Separate betas
-    betaMC <- betaOut[1:(C*n_basis)]
-    betaMtx <- cbind(beta=as.matrix(betaMC),
-                     type=rep(1:C, each=n_basis))
-    mcMtx <- tapply(betaMtx[,1],
-                    betaMtx[,2],
-                    function(x) B %*% x)
-    mcMtx <- data.frame(mc=unlist(mcMtx),
-                        time=rep(tuni,times=C),
-                        type=rep(unique(market[,2]), each=length(tuni)))
+    if(!is.null(timeVar2)){
+        if(basisFunction=='B-Splines')
+            basisObj = create.bspline.basis(range(t),
+                                            nbasis = n_basis,
+                                            norder = n_order)
+        if(basisFunction=='Fourier'){
+            basisObj = create.fourier.basis(range(t),
+                                            nbasis = n_basis)
+        }
+        tuni <- unique(t)
+        ## Second component
+        n_basis2 <- ifelse(is.null(n_basis2),n_basis,n_basis2)
+        basis2 <- ifelse(is.null(basisFunction2),basisFunction,basisFunction2)
+        n_order2 <- ifelse(is.null(n_order2),n_order,n_order2)
+        tuni2 <- unique(t2)
+        if(basis2=='B-Splines')
+            basisObj2 = create.bspline.basis(range(tuni2),
+                                            nbasis = n_basis2,
+                                            norder = n_order2)
+        if(basis2=='Fourier'){
+            basisObj2 = create.fourier.basis(range(tuni2),
+                                            nbasis = n_basis2)
+        }
+        t1len <- length(tuni)
+        t2len <- length(tuni2)
+        t1 <- rep(tuni, times = t2len)
+        t2 <- rep(tuni2, each = t1len)
+        B1 = predict(basisObj, t1)
+        B2 = predict(basisObj2, t2)
+        nc <- ncol(B1)
+        BB2 <- cbind(B1,B2)
+        B <- apply(BB2,1,function(x) x[c(1:nc)] %x% x[-c(1:nc)])
+        B <- t(B)
+        rm(BB2)
+        ## Separate betas
+        L <- ncol(B2)
+        betaMC <- betaOut[1:(C*L*n_basis)]
+        betaMtx <- cbind(beta=as.matrix(betaMC),
+                         type=rep(1:C, each=L*n_basis))
+        mcMtx <- tapply(betaMtx[,1],
+                        betaMtx[,2],
+                        function(x) B %*% x)
+        mcMtx <- data.frame(time=rep(t1,times=C),
+                            time2=rep(t2,times=C),
+                            type=rep(unique(market[,2]), each=length(t1)),
+                            mc=unlist(mcMtx))
+        # colnames(mcMtx) <- c(timeVar,timeVar2,type,mc)
+    }
     ## Output data with predicted values
     dd$pred <- as.numeric(X %*% betaOut)
     dd$group <- as.factor(dd$group)
@@ -312,7 +376,7 @@ aggrmodel <- function(formula=NULL,
 ##' Aux function to get initial values in aggrmodel function
 ##'
 ##' @title get_inits
-##' @param XList Design matrix list
+##' @param X Design matrix
 ##' @param I Number of replicates
 ##' @param y Dependent variable
 ##' @param C Number of subject types
@@ -328,7 +392,7 @@ aggrmodel <- function(formula=NULL,
 ##' @param basisFunc Basis function type
 ##' @return An object with initial values
 ##' @author Gabriel Franco
-get_inits <- function(XList, I, y, C,
+get_inits <- function(X, I, y, C,
                       covType,
                        market,
                        corPar_init, tauPar_init,
@@ -337,14 +401,14 @@ get_inits <- function(XList, I, y, C,
                        t, n_order, basisFunc
                        ){
 
-    X <- lapply(XList,
-                function(x)
-                    do.call(rbind,replicate(n=I,
-                                            expr=x,
-                                            simplify=FALSE)
-                            )
-                )
-    X <- do.call(rbind, X)
+    # X <- lapply(XList,
+    #             function(x)
+    #                 do.call(rbind,replicate(n=I,
+    #                                         expr=x,
+    #                                         simplify=FALSE)
+    #                         )
+    #             )
+    # X <- do.call(rbind, X)
     ## Get beta init -----------------------------
     ddfit_init <- data.frame(y=y,X)
     fit_init <- lm(y~.-1, data = ddfit_init)
